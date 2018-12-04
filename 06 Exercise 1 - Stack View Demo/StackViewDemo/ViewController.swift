34850de3b4350437669508f3fc733433c7b6dbb7
Razmanugget <rami_s@hotmail.com>
2018-12-04 18:22:33 +0100

    finished Swift 4.2

---
+++ 06 Exercise 1 - Stack View Demo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Rami on 11/30/18.
+//  Copyright © 2018 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
