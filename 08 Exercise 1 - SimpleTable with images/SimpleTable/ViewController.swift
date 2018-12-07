75280f6d6715dc4ec6ebef677da1d582a3a310f9
Razmanugget <rami_s@hotmail.com>
2018-12-07 21:58:45 +0100

    finished Swift 4.2

---
+++ 08 Exercise 1 - SimpleTable with images/SimpleTable/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Rami on 12/1/18.
+//  Copyright © 2018 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Enums | Extensions
+// MARK: - IBActions
+
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  // MARK: - Variables
+  var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+  var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+  
+  
+  
+  // MARK: - Functions
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return restaurantNames.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cellIdentifier = "datacell"
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+    
+    // configure the cell
+    cell.textLabel?.text = restaurantNames[indexPath.row]
+    cell.imageView?.image = UIImage(named: restaurantImages[indexPath.row])
+    
+    return cell
+  }
+  
+  
+  
+  // MARK: - Override Functions
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+}
+
