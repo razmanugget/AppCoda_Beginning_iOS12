d1622b82b1d16fbabc1c49271f8592609b6a0aba
Razmanugget <rami_s@hotmail.com>
2018-12-08 17:20:02 +0100

    finished Swift 4.2, cleaned up

--- 09 Exercise 1 - FoodPin more data/FoodPin/Controllers/RestaurantTableViewController.swift
+++ 09 Exercise 1 - FoodPin more data/FoodPin/Controllers/RestaurantTableViewController.swift
@@ -22,6 +22,7 @@ class RestaurantTableViewController: UITableViewController {
   
   var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
   
+  
   // MARK: - Functions
 
   
@@ -45,56 +46,13 @@ class RestaurantTableViewController: UITableViewController {
     cell.locationLabel.text = restaurantLocations[indexPath.row]
     cell.typeLabel.text = restaurantTypes[indexPath.row]
     cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
-    
     return cell
   }
-  
-  
-  /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
-  
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
-  
-  /*
-   // Override to support rearranging the table view.
-   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-   
-   }
-   */
-  
-  /*
-   // Override to support conditional rearranging of the table view.
-   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the item to be re-orderable.
-   return true
-   }
-   */
-  
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem
   }
   
 }
