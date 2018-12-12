035006cf4e9c87fd993278f937e7f1df3ab4f0b4
Razmanugget <rami_s@hotmail.com>
2018-12-12 16:46:30 +0100

    modified initial commit

--- 14 Detail View Enhancement, Custom Fonts, Self Sizing Cells/FoodPin/Models/Restaurant.swift
+++ 14 Detail View Enhancement, Custom Fonts, Self Sizing Cells/FoodPin/Models/Restaurant.swift
@@ -12,18 +12,22 @@ class Restaurant {
   var name: String
   var type: String
   var location: String
+  var phone: String
+  var description: String
   var image: String
   var isVisited: Bool
   
-  init(name: String, type: String, location: String, image: String, isVisited: Bool) {
+  init(name: String, type: String, location: String, phone: String, description: String, image: String, isVisited: Bool) {
     self.name = name
     self.type = type
     self.location = location
+    self.phone = phone
+    self.description = description
     self.image = image
     self.isVisited = isVisited
   }
   
   convenience init() {
-    self.init(name: "", type: "", location: "", image: "", isVisited: false)
+    self.init(name: "", type: "", location: "", phone: "", description: "", image: "", isVisited: false)
   }
 }
