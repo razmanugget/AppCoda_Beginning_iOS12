204f7a53b20bc4ece807eb4aed3217a71b9ea4d4
Razmanugget <rami_s@hotmail.com>
2018-12-20 11:21:21 +0100

    initial commit

---
+++ 18 Static Table Views, Camera, NSLayoutConstraint/FoodPin/Views/RestaurantDetailTextCell.swift
@@ -0,0 +1,31 @@
+//
+//  RestaurantDetailTextCell.swift
+//  FoodPin
+//
+//  Created by Rami on 12/13/18.
+//  Copyright © 2018 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailTextCell: UITableViewCell {
+  
+  @IBOutlet var descriptionLabel: UILabel! {
+    didSet {
+      descriptionLabel.numberOfLines = 0
+    }
+  }
+  
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
