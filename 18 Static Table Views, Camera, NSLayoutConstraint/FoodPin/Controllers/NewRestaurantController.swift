9e4aacda77c62dbc3d03f93242024cfb269ee515
Razmanugget <rami_s@hotmail.com>
2018-12-21 14:04:18 +0100

    update fix

--- 18 Static Table Views, Camera, NSLayoutConstraint/FoodPin/Controllers/NewRestaurantController.swift
+++ 18 Static Table Views, Camera, NSLayoutConstraint/FoodPin/Controllers/NewRestaurantController.swift
@@ -66,8 +66,7 @@ class NewRestaurantController: UITableViewController, UITextFieldDelegate {
     // Uncomment the following line to preserve selection between presentations
     // self.clearsSelectionOnViewWillAppear = false
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
   }
   
   // MARK: - Table view data source
