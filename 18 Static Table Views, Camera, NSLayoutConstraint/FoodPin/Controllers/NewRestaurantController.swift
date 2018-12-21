bebf22f2cdde5fbfa4fddc595009d9a856f0fa82
Razmanugget <rami_s@hotmail.com>
2018-12-21 18:14:02 +0100

    finished Swift 4.2

--- 18 Static Table Views, Camera, NSLayoutConstraint/FoodPin/Controllers/NewRestaurantController.swift
+++ 18 Static Table Views, Camera, NSLayoutConstraint/FoodPin/Controllers/NewRestaurantController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NewRestaurantController: UITableViewController, UITextFieldDelegate {
+class NewRestaurantController: UITableViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   @IBOutlet var nameTextField: RoundedTextField! {
     didSet {
       nameTextField.tag = 1
@@ -46,6 +46,7 @@ class NewRestaurantController: UITableViewController, UITextFieldDelegate {
     }
   }
   
+  @IBOutlet var photoImageView: UIImageView!
   
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
     if let nextTextField = view.viewWithTag(textField.tag + 1) {
@@ -57,18 +58,72 @@ class NewRestaurantController: UITableViewController, UITextFieldDelegate {
   }
   
   
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+      photoImageView.image = selectedImage
+      photoImageView.contentMode = .scaleAspectFill
+      photoImageView.clipsToBounds = true
+    }
+    
+    let leadingConstraint = NSLayoutConstraint(item: photoImageView, attribute: .leading, relatedBy: .equal, toItem: photoImageView.superview, attribute: .leading, multiplier: 1, constant: 0)
+    leadingConstraint.isActive = true
+    
+    let trailingConstraint = NSLayoutConstraint(item: photoImageView, attribute: .trailing, relatedBy: .equal, toItem: photoImageView.superview, attribute: .trailing, multiplier: 1, constant: 0)
+    trailingConstraint.isActive = true
+    
+    let topConstraint = NSLayoutConstraint(item: photoImageView, attribute: .top, relatedBy: .equal, toItem: photoImageView.superview, attribute: .top, multiplier: 1, constant: 0)
+    topConstraint.isActive = true
+    
+    let bottomConstraint = NSLayoutConstraint(item: photoImageView, attribute: .bottom, relatedBy: .equal, toItem: photoImageView.superview, attribute: .bottom, multiplier: 1, constant: 0)
+    bottomConstraint.isActive = true
+    
+    dismiss(animated: true, completion: nil)
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if indexPath.row == 0 {
+      let photoSourceRequestController = UIAlertController(title: "", message: "Choose your photo source", preferredStyle: .actionSheet)
+      let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {(action) in
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+          let imagePicker = UIImagePickerController()
+          imagePicker.allowsEditing = false
+          imagePicker.sourceType = .camera
+          imagePicker.delegate = self
+          self.present(imagePicker, animated: true, completion: nil)
+        }
+      })
+      let photoLibraryAction = UIAlertAction(title: "Photo library", style: .default, handler: {(action) in
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+          let imagePicker = UIImagePickerController()
+          imagePicker.allowsEditing = false
+          imagePicker.sourceType = .photoLibrary
+          imagePicker.delegate = self
+          self.present(imagePicker, animated: true, completion: nil)
+        }
+      })
+      photoSourceRequestController.addAction(cameraAction)
+      photoSourceRequestController.addAction(photoLibraryAction)
+      
+      // for ipad
+      if let popoverController = photoSourceRequestController.popoverPresentationController {
+        if let cell = tableView.cellForRow(at: indexPath) {
+          popoverController.sourceView = cell
+          popoverController.sourceRect = cell.bounds
+        }
+      }
+      present(photoSourceRequestController, animated: true, completion: nil)
+    }
+  }
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    tableView.separatorStyle = .none
-    
-    
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
+    //    tableView.separatorStyle = .none
     
   }
-  
-  // MARK: - Table view data source
   
 }
