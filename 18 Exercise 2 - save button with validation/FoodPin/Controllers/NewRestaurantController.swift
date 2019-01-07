2dabd1501a80fba77ec53a4c13774577ac5eed90
Razmanugget <rami_s@hotmail.com>
2019-01-07 15:26:54 +0100

    finished Swift 4.2

--- 18 Exercise 2 - save button with validation/FoodPin/Controllers/NewRestaurantController.swift
+++ 18 Exercise 2 - save button with validation/FoodPin/Controllers/NewRestaurantController.swift
@@ -9,73 +9,63 @@
 import UIKit
 
 class NewRestaurantController: UITableViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-  @IBOutlet var nameTextField: RoundedTextField! {
-    didSet {
-      nameTextField.tag = 1
-      nameTextField.becomeFirstResponder()
-      nameTextField.delegate = self
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet var nameTextField: RoundedTextField! {
+        didSet {
+            nameTextField.tag = 1
+            nameTextField.becomeFirstResponder()
+            nameTextField.delegate = self
+        }
     }
-  }
-  
-  @IBOutlet var typeTextField: RoundedTextField! {
-    didSet {
-      typeTextField.tag = 2
-      typeTextField.delegate = self
+    
+    @IBOutlet var typeTextField: RoundedTextField! {
+        didSet {
+            typeTextField.tag = 2
+            typeTextField.delegate = self
+        }
     }
-  }
-  
-  @IBOutlet var addressTextField: RoundedTextField! {
-    didSet {
-      addressTextField.tag = 3
-      addressTextField.delegate = self
+    
+    @IBOutlet var addressTextField: RoundedTextField! {
+        didSet {
+            addressTextField.tag = 3
+            addressTextField.delegate = self
+        }
     }
-  }
-  
-  @IBOutlet var phoneTextField: RoundedTextField! {
-    didSet {
-      phoneTextField.tag = 4
-      phoneTextField.delegate = self
+    
+    @IBOutlet var phoneTextField: RoundedTextField! {
+        didSet {
+            phoneTextField.tag = 4
+            phoneTextField.delegate = self
+        }
     }
-  }
-  
-  @IBOutlet var descriptionTextView: UITextView! {
-    didSet {
-      descriptionTextView.tag = 5
-      descriptionTextView.layer.cornerRadius = 5.0
-      descriptionTextView.layer.masksToBounds = true
+    
+    @IBOutlet var descriptionTextView: UITextView! {
+        didSet {
+            descriptionTextView.tag = 5
+            descriptionTextView.layer.cornerRadius = 5.0
+            descriptionTextView.layer.masksToBounds = true
+        }
     }
-  }
-  
-  @IBOutlet var photoImageView: UIImageView!
-  
+    
+    @IBOutlet var photoImageView: UIImageView!
+    
     
     @IBAction func saveButtonTapped(sender: AnyObject) {
-        self.validateInput()
+        let errorType = self.validateInput()
         
-//        var errorType = ""
-//
-//        if nameTextField.text == "" {
-//            errorType = "name"
-//        } else if typeTextField.text == "" {
-//            errorType = "type"
-//        } else if addressTextField.text == "" {
-//            errorType = "address"
-//        } else if phoneTextField.text == "" {
-//            errorType = "phone"
-//        } else if descriptionTextView.text == "" {
-//            errorType = "description"
-//        }
-//
-//        if errorType != "" {
-//        let alertController =
-//            UIAlertController(title: "Oops", message: "We can't continue because you need to fill in the \(errorType) field", preferredStyle: .alert)
-//        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-//        alertController.addAction(alertAction)
-//        present(alertController, animated: true, completion: nil)
-//
-//            return
-//        }
-       
+        if errorType != "" {
+            let alertController =
+                UIAlertController(title: "Oops", message:
+                    "We can't continue because you need to fill in the \(errorType) field",
+                    preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(alertAction)
+            present(alertController, animated: true, completion: nil)
+            
+            return
+        }
         print("Name: \(nameTextField.text ?? "")")
         print("Type: \(typeTextField.text ?? "")")
         print("Location: \(addressTextField.text ?? "")")
@@ -86,121 +76,109 @@ class NewRestaurantController: UITableViewController, UITextFieldDelegate, UIIma
     }
     
     
-    
     // MARK: - UITextFieldDelegate methods
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if let nextTextField = view.viewWithTag(textField.tag + 1) {
-      textField.resignFirstResponder()
-      nextTextField.becomeFirstResponder()
+            textField.resignFirstResponder()
+            nextTextField.becomeFirstResponder()
+        }
+        return true
     }
-    return true
-  }
-  
-  
-  
-  func validateInput() {
-    var errorType = ""
-
-    if nameTextField.text == "" {
-        errorType = "name"
-    } else if typeTextField.text == "" {
-        errorType = "type"
-    } else if addressTextField.text == "" {
-        errorType = "address"
-    } else if phoneTextField.text == "" {
-        errorType = "phone"
-    } else if descriptionTextView.text == "" {
-        errorType = "description"
-    }
-
-    if errorType != "" {
-        let alertController =
-            UIAlertController(title: "Oops", message:
-                "We can't continue because you need to fill in the \(errorType) field",
-                preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(alertAction)
-        present(alertController, animated: true, completion: nil)
+    
+    
+    func validateInput() -> String {
+        var error = ""
         
-        return
-    }
-  }
-  
-  
-  // MARK: - UIImagePickerControllerDelegate methods
-  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-    if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-      photoImageView.image = selectedImage
-      photoImageView.contentMode = .scaleAspectFill
-      photoImageView.clipsToBounds = true
-    }
-    
-    let leadingConstraint = NSLayoutConstraint(item: photoImageView, attribute: .leading, relatedBy: .equal, toItem: photoImageView.superview, attribute: .leading, multiplier: 1, constant: 0)
-    leadingConstraint.isActive = true
-    
-    let trailingConstraint = NSLayoutConstraint(item: photoImageView, attribute: .trailing, relatedBy: .equal, toItem: photoImageView.superview, attribute: .trailing, multiplier: 1, constant: 0)
-    trailingConstraint.isActive = true
-    
-    let topConstraint = NSLayoutConstraint(item: photoImageView, attribute: .top, relatedBy: .equal, toItem: photoImageView.superview, attribute: .top, multiplier: 1, constant: 0)
-    topConstraint.isActive = true
-    
-    let bottomConstraint = NSLayoutConstraint(item: photoImageView, attribute: .bottom, relatedBy: .equal, toItem: photoImageView.superview, attribute: .bottom, multiplier: 1, constant: 0)
-    bottomConstraint.isActive = true
-    
-    dismiss(animated: true, completion: nil)
-  }
-  
-  
-  // MARK: - UITableViewDelegate methods
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    if indexPath.row == 0 {
-      let photoSourceRequestController = UIAlertController(title: "", message: "Choose your photo source", preferredStyle: .actionSheet)
-      let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {(action) in
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-          let imagePicker = UIImagePickerController()
-          imagePicker.allowsEditing = false
-          imagePicker.sourceType = .camera
-          imagePicker.delegate = self
-          self.present(imagePicker, animated: true, completion: nil)
+        if nameTextField.text == "" {
+            error = "name"
+        } else if typeTextField.text == "" {
+            error = "type"
+        } else if addressTextField.text == "" {
+            error = "address"
+        } else if phoneTextField.text == "" {
+            error = "phone"
+        } else if descriptionTextView.text == "" {
+            error = "description"
         }
-      })
-      let photoLibraryAction = UIAlertAction(title: "Photo library", style: .default, handler: {(action) in
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-          let imagePicker = UIImagePickerController()
-          imagePicker.allowsEditing = false
-          imagePicker.sourceType = .photoLibrary
-          imagePicker.delegate = self
-          self.present(imagePicker, animated: true, completion: nil)
-        }
-      })
-      photoSourceRequestController.addAction(cameraAction)
-      photoSourceRequestController.addAction(photoLibraryAction)
-      
-      // for ipad
-      if let popoverController = photoSourceRequestController.popoverPresentationController {
-        if let cell = tableView.cellForRow(at: indexPath) {
-          popoverController.sourceView = cell
-          popoverController.sourceRect = cell.bounds
-        }
-      }
-      present(photoSourceRequestController, animated: true, completion: nil)
-    }
-  }
-  
-  
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    //    tableView.separatorStyle = .none
-    
-    // Configure navigation bar appearance
-    navigationController?.navigationBar.tintColor = .white
-    navigationController?.navigationBar.shadowImage = UIImage()
-    if let customFont = UIFont(name: "Rubik-Medium", size: 35.0) {
-      navigationController?.navigationBar.largeTitleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont ]
+        return error
     }
     
-  }
-  
+    
+    // MARK: - UIImagePickerControllerDelegate methods
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            photoImageView.image = selectedImage
+            photoImageView.contentMode = .scaleAspectFill
+            photoImageView.clipsToBounds = true
+        }
+        
+        let leadingConstraint = NSLayoutConstraint(item: photoImageView, attribute: .leading, relatedBy: .equal, toItem: photoImageView.superview, attribute: .leading, multiplier: 1, constant: 0)
+        leadingConstraint.isActive = true
+        
+        let trailingConstraint = NSLayoutConstraint(item: photoImageView, attribute: .trailing, relatedBy: .equal, toItem: photoImageView.superview, attribute: .trailing, multiplier: 1, constant: 0)
+        trailingConstraint.isActive = true
+        
+        let topConstraint = NSLayoutConstraint(item: photoImageView, attribute: .top, relatedBy: .equal, toItem: photoImageView.superview, attribute: .top, multiplier: 1, constant: 0)
+        topConstraint.isActive = true
+        
+        let bottomConstraint = NSLayoutConstraint(item: photoImageView, attribute: .bottom, relatedBy: .equal, toItem: photoImageView.superview, attribute: .bottom, multiplier: 1, constant: 0)
+        bottomConstraint.isActive = true
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - UITableViewDelegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            let photoSourceRequestController = UIAlertController(title: "", message: "Choose your photo source", preferredStyle: .actionSheet)
+            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {(action) in
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    let imagePicker = UIImagePickerController()
+                    imagePicker.allowsEditing = false
+                    imagePicker.sourceType = .camera
+                    imagePicker.delegate = self
+                    self.present(imagePicker, animated: true, completion: nil)
+                }
+            })
+            let photoLibraryAction = UIAlertAction(title: "Photo library", style: .default, handler: {(action) in
+                if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                    let imagePicker = UIImagePickerController()
+                    imagePicker.allowsEditing = false
+                    imagePicker.sourceType = .photoLibrary
+                    imagePicker.delegate = self
+                    self.present(imagePicker, animated: true, completion: nil)
+                }
+            })
+            photoSourceRequestController.addAction(cameraAction)
+            photoSourceRequestController.addAction(photoLibraryAction)
+            
+            // for ipad
+            if let popoverController = photoSourceRequestController.popoverPresentationController {
+                if let cell = tableView.cellForRow(at: indexPath) {
+                    popoverController.sourceView = cell
+                    popoverController.sourceRect = cell.bounds
+                }
+            }
+            present(photoSourceRequestController, animated: true, completion: nil)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.separatorStyle = .none
+        
+        // Configure navigation bar appearance
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.shadowImage = UIImage()
+        if let customFont = UIFont(name: "Rubik-Medium", size: 35.0) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont ]
+        }
+    }
+    
 }
