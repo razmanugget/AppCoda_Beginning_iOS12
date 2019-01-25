1e452971411767fff0fe307f0d063aab7e016507
Razmanugget <rami_s@hotmail.com>
2019-01-25 17:12:23 +0100

    initial commit

---
+++ 19 Exercise 1 - clean up the description field/FoodPin/Views/RestaurantDetailHeaderView.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by Rami on 12/13/18.
+//  Copyright © 2018 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var backgroundTypeLabel: UILabel!
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var heartImageView: UIImageView! {
+        didSet {
+            heartImageView.image = UIImage(
+                named: "heart-tick")?.withRenderingMode(.alwaysTemplate)
+            heartImageView.tintColor = .white
+        }
+    }
+    @IBOutlet var ratingImageView: UIImageView!
+
+}
---
+++ 19 Exercise 1 - clean up the description field/FoodPin/Views/RestaurantTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Rami on 12/3/18.
+//  Copyright © 2018 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var backgroundTypeLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+    @IBOutlet weak var heartImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+        
+    }
+}
