759e621a65b84ddccd4a496dff1ae2eb50893eb1
Razmanugget <rami_s@hotmail.com>
2019-02-14 16:17:51 +0100

    wip

---
+++ 21 Building Walkthrough Screens/FoodPin/Controllers/WalkthroughContentViewController.swift
@@ -0,0 +1,50 @@
+//
+//  WalkthroughContentViewController.swift
+//  FoodPin
+//
+//  Created by Rami on 2/13/19.
+//  Copyright © 2019 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughContentViewController: UIViewController {
+    @IBOutlet var headingLabel: UILabel! {
+        didSet {
+            headingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var subHeadingLabel: UILabel! {
+        didSet {
+            subHeadingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var contentImageView: UIImageView!
+    
+    var index = 0
+    var heading = ""
+    var subHeading = ""
+    var imageFile = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        headingLabel.text = heading
+        subHeadingLabel.text = subHeading
+        contentImageView.image = UIImage(named: imageFile)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
