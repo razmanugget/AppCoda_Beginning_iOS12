71d5f8594ccc04105415064e92c21f8db3d84734
Razmanugget <rami_s@hotmail.com>
2019-02-18 18:30:48 +0100

    initial commit

---
+++ 22 Exploring Tab Bar Controller and Story Ref/FoodPin/Controllers/WalkthroughContentViewController.swift
@@ -0,0 +1,40 @@
+//
+//  WalkthroughContentViewController.swift
+//  FoodPin
+//
+//  Created by Rami on 2/13/19.
+//  Copyright © 2019 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughContentViewController: UIViewController {
+    
+    @IBOutlet var headingLabel: UILabel! {
+        didSet {
+            headingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var subHeadingLabel: UILabel! {
+        didSet {
+            subHeadingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var contentImageView: UIImageView!
+    
+    var index = 0
+    var heading = ""
+    var subHeading = ""
+    var imageFile = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        headingLabel.text = heading
+        subHeadingLabel.text = subHeading
+        contentImageView.image = UIImage(named: imageFile)
+    }
+    
+}
