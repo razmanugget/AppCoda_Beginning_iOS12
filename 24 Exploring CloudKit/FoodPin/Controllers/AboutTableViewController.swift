ace6595667929ce1577a2e3118d240fd815f9dd4
Razmanugget <rami_s@hotmail.com>
2019-02-22 11:50:07 +0100

    initial commit

---
+++ 24 Exploring CloudKit/FoodPin/Controllers/AboutTableViewController.swift
@@ -0,0 +1,132 @@
+//
+//  AboutTableViewController.swift
+//  FoodPin
+//
+//  Created by Rami on 2/20/19.
+//  Copyright © 2019 LyfeBug. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class AboutTableViewController: UITableViewController {
+    // MARK: - Variables
+    var sectionTitles = ["Feedback", "Follow Us"]
+    var sectionContent = [
+        [
+            (image: "store", 
+             text: "Rate us on the App Store", 
+             link: "https://www.apple.com/itunes/charts/paid-apps/"),
+            (image: "chat", 
+             text: "Tell us your feedback", 
+             link: "http://www.appcoda.com/contact")
+        ],
+        [
+            (image: "twitter", 
+             text: "Twitter", 
+             link: "https://twitter.com/appcodamobile"),
+            (image: "facebook", 
+             text: "Facebook", 
+             link: "https://facebook.com/appcodamobile"),
+            (image: "instagram", 
+             text: "Instagram", 
+             link: "https://www.instagram.com/appcodadotcom")
+        ]
+    ]
+    
+    // MARK: - Override Functions
+    
+    override func numberOfSections(
+        in tableView: UITableView) 
+        -> Int {
+            
+            return sectionTitles.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView, 
+        numberOfRowsInSection section: Int) 
+        -> Int {
+            
+            return sectionContent[section].count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView, 
+        titleForHeaderInSection section: Int)
+        -> String? {
+            
+            return sectionTitles[section]
+    }
+    
+    override func tableView(
+        _ tableView: UITableView, 
+        cellForRowAt indexPath: IndexPath) 
+        -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "AboutCell", 
+                for: indexPath)
+            
+            // configure the cell
+            let cellData = sectionContent[indexPath.section][indexPath.row]
+            cell.textLabel?.text = cellData.text
+            cell.imageView?.image = UIImage(named: cellData.image)
+            
+            return cell
+    }
+    
+    override func tableView(
+        _ tableView: UITableView, 
+        didSelectRowAt indexPath: IndexPath) {
+        
+        let link = sectionContent[indexPath.section][indexPath.row].link
+        
+        switch indexPath.section {
+            // leave us feedback section
+        case 0:
+            if indexPath.row == 0 {
+                if let url = URL(string: link) {
+                    UIApplication.shared.open(url)
+                }
+            } else if indexPath.row == 1 {
+                performSegue(withIdentifier: "showWebView", sender: self)
+            }
+            // follow us section
+        case 1:
+            if let url = URL(string: link) {
+                let safariController = SFSafariViewController(url: url)
+                present(safariController, animated: true, completion: nil)
+            }
+        default:
+            break
+        }
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showWebView" {
+            if let destinationController = segue.destination as? WebViewController, let indexPath = tableView.indexPathForSelectedRow {
+                destinationController.targetURL = sectionContent[indexPath.section][indexPath.row].link
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // configure the navigation bar appearance
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [ 
+                NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), 
+                NSAttributedString.Key.font: customFont ]
+        }
+        tableView.tableFooterView = UIView()
+    }
+    
+}
---
+++ 24 Exploring CloudKit/FoodPin/Controllers/MapViewController.swift
@@ -0,0 +1,84 @@
+//
+//  MapViewController.swift
+//  FoodPin
+//
+//  Created by Rami on 12/17/18.
+//  Copyright © 2018 LyfeBug. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, 
+MKMapViewDelegate {
+    // MARK: - Variables
+    var restaurant: RestaurantMO!
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    // MARK: - MKMapViewDelegate methods
+
+    func mapView(
+        _ mapView: MKMapView,
+        viewFor annotation: MKAnnotation)
+        -> MKAnnotationView? {
+            
+            let identifier = "MyMarker"
+            
+            if annotation.isKind(of: MKUserLocation.self) {
+                return nil
+            }
+
+            // Reuse the annotation if possible
+            var annotationView: MKMarkerAnnotationView? =
+                mapView.dequeueReusableAnnotationView(
+                    withIdentifier: identifier) as? MKMarkerAnnotationView
+
+            if annotationView == nil {
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            }
+
+            annotationView?.glyphText = "😋"
+            annotationView?.markerTintColor = UIColor.orange
+
+            return annotationView
+    }
+
+    // MARK: - View controller life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Customize the map view
+        mapView.delegate = self
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        mapView.showsTraffic = true
+
+        // Convert address to coordinate and annotate it on map
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.location ?? "", completionHandler: { placemarks, error in
+            if let error = error {
+                print(error)
+                return
+            }
+
+            if let placemarks = placemarks {
+                // Get the first placemark
+                let placemark = placemarks[0]
+
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.type
+
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    
+                    // Display the annotation
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+        })
+    }
+}
