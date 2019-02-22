15e5ca37add4eb71eb0d2fa0975f51e82934913b
Razmanugget <rami_s@hotmail.com>
2019-02-22 15:05:35 +0100

    initial commit

---
+++ 25 Localizing the App/FoodPin/Controllers/WalkthroughViewController.swift
@@ -0,0 +1,83 @@
+//
+//  WalkthroughViewController.swift
+//  FoodPin
+//
+//  Created by Rami on 2/15/19.
+//  Copyright © 2019 LyfeBug. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughViewController: UIViewController, 
+WalkthroughPageViewControllerDelegate {
+    // MARK: - Variables
+    var walkthroughPageViewController: WalkthroughPageViewController?
+    
+    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet var nextButton: UIButton! {
+        didSet {
+            nextButton.layer.cornerRadius = 25.0
+            nextButton.layer.masksToBounds = true
+        }
+    }
+    @IBOutlet var skipButton: UIButton!
+    
+    @IBAction func skipButtonTapped(sender: UIButton) {
+        UserDefaults.standard.set(true, forKey: "hasViewedWalkthrough")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func nextButtonTapped(
+        sender: UIButton) {
+        
+        if let index = walkthroughPageViewController?.currentIndex {
+            switch index {
+            case 0...1:
+                walkthroughPageViewController?.forwardPage()
+            case 2:
+                UserDefaults.standard.set(true, forKey: "hasViewedWalkthrough")
+                dismiss(animated: true, completion: nil)
+            default:
+                break
+            }
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        if let index = walkthroughPageViewController?.currentIndex {
+            switch index {
+            case 0...1:
+                nextButton.setTitle("NEXT", for: .normal)
+                skipButton.isHidden = false
+            case 2:
+                nextButton.setTitle("GET STARTED", for: .normal)
+                skipButton.isHidden = true
+            default:
+                break
+            }
+            pageControl.currentPage = index
+        }
+    }
+    
+    func didUpdatePageIndex(currentIndex: Int) {
+        updateUI()
+    }
+    
+    override func prepare(
+        for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let destination = segue.destination
+        if let pageViewController = destination as? WalkthroughPageViewController {
+            walkthroughPageViewController = pageViewController
+            
+            walkthroughPageViewController?.walkthroughDelegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
